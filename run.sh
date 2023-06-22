e542b029a6ee558ebbd95474a2e5698bb21f6e92
aryansingh920 <aryansingh920@outlook.com>
2023-06-22 12:55:41 +0530

    2023-06-22 12:55:41

--- run.sh
+++ run.sh
@@ -6,8 +6,24 @@ current_dir=$(pwd)
 export PORT_CLIENT=3001
 export PORT_SERVER=3002
 
-# Command to run run.sh in a new terminal
+# Command to run run.sh in a new terminal for the client
 osascript -e "tell application \"Terminal\" to do script \"cd $current_dir/client/ && bash run.sh\""
 
-# Command to run another run.sh in a new terminal
+# Delay for 2 seconds
+sleep 2
+
+# Command to run run.sh in a new terminal for the server
 osascript -e "tell application \"Terminal\" to do script \"cd $current_dir/server/ && bash run.sh\""
+
+
+cleanup() {
+  # Unset the exported variables
+  unset PORT_CLIENT
+  unset PORT_SERVER
+
+  # Exit the script
+  exit 1
+}
+
+# Set up the trap to execute the cleanup function on SIGINT signal (Ctrl+C)
+trap cleanup SIGINT
